c25256f7fde4981ae77e899310377488697629d9
nicode8 <nicodili2008@gmail.com>
2025-10-23 12:42:18 +0200

    commit

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
 $DB_host= "db.fr-pari1.bengt.wasmernet.com";
-$DB_name= "database_dili2";
-$DB_user= "a00d74d672308000037c21f1be27";
-$DB_pass= "068fa00d-74d6-7449-8000-5fd0cb1dbd8f";
+$DB_name= "database_dili08";
+$DB_user= "a064c5737d5780004c02b33a2540";
+$DB_pass= "068fa064-c573-7ee9-8000-a268bf5ccac9";
 
 
 ?>

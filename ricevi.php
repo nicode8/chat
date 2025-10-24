24e5a917206e14fe5aacca920299143dc8b9e5fd
nicode8 <nicodili2008@gmail.com>
2025-10-24 23:56:37 +0200

    commit

--- ricevi.php
+++ ricevi.php
@@ -1,5 +1,4 @@
 <?php
-
 session_start();
 require 'config.php';
 $conn= new mysqli($DB_host,$DB_user,$DB_pass,$DB_name);
@@ -8,10 +7,10 @@ if($conn->connect_error)
     die("error".$conn->error);
 
 
-$sql="SELECT mittente,messaggio from messaggi_inviati where ricevente=?";
+$sql="SELECT messaggio from messaggi_inviati where ricevente=? AND mittente=?";
 
 $stmt= $conn->prepare($sql);
-$stmt->bind_param("s",$_SESSION["mittente"]);
+$stmt->bind_param("ss",$_SESSION["email"],$_SESSION["destinatario"]);
 $stmt->execute();
 $result=$stmt->get_result();
 

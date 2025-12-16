73404e877afbc6d5f85a381fd65962a7c256373d
nicode8 <nicodili2008@gmail.com>
2025-12-16 21:14:29 +0100

    check

--- accesso.php
+++ accesso.php
@@ -1,13 +1,17 @@
 <?php
 session_start();
+header('Content-Type: application/json');
 require 'config.php';
 $conn= new mysqli($DB_host,$DB_user,$DB_pass,$DB_name);
 
 if($conn->connect_error)
-    echo json_decode("error");
+    echo json_encode($conn->connect_error);
 
-$email=$_POST["email"];
-$password=$_POST["password"];
+$json=file_get_contents("php://input");
+$call_api=json_decode($json,true);
+
+$email=$call_api["email"];
+$password=$call_api["password"];
 
 
 $sql="SELECT * FROM accessi WHERE email=? AND passw=? ";
@@ -18,28 +22,22 @@ $stmt->execute();
 $result= $stmt->get_result();
 
 
-if($row= $result->fetch_assoc())
-{   $_SESSION["email"]=$_POST["email"];
-    header("Location: destinatario.html");
+if($result->fetch_assoc())
+{   $_SESSION["email"]=$email;
+    echo json_encode(["success"=>true,"message"=>"fatto"]);
+
+ 
 }
-else
+/*else
 {
     header("location:index.html");
     echo"<script type='text/javascript> alert('questo account non esiste');</script>";
 
-}
+}*/
 
 
 $stmt->close();
 $conn->close();
-
-
-
-
-
-
-
-
 
 
 
--- ricevi.php
+++ ricevi.php
@@ -1,27 +1,40 @@
 <?php
 session_start();
+header('Content-Type: application/json');
 require 'config.php';
 $conn= new mysqli($DB_host,$DB_user,$DB_pass,$DB_name);
 
 if($conn->connect_error)
     die("error".$conn->error);
 
+$json=file_get_contents("php://input");
+$json=json_decode($json,true);
 
-$sql="SELECT messaggio from messaggi_inviati where ricevente=? AND mittente=?";
+$id=$json;
+
+
+
+$sql="SELECT id,messaggio from messaggi_inviati where ricevente=? AND mittente=? AND id>?";
 
 $stmt= $conn->prepare($sql);
-$stmt->bind_param("ss",$_SESSION["email"],$_SESSION["destinatario"]);
+$stmt->bind_param("ssi",$_SESSION["email"],$_SESSION["destinatario"],$id);
 $stmt->execute();
 $result=$stmt->get_result();
 
 $messaggio=[];
 while($row=$result->fetch_assoc())
 {
-    $messaggio[]=$row["messaggio"];
+    $results[]=[
+        "id"=>$row["id"],
+        "messaggio"=>$row["messaggio"]
+    ];
 
 }
-header('Content-Type: application/json');
-echo json_encode($messaggio);
+echo json_encode($results);
+
+
+
+
 
 
 
